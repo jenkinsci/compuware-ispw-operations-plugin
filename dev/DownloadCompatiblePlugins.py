5e26d02013d01fc1b49658a8242f24d02dcfdafe
Administrator <Administrator@172.19.173.150>
2025-04-28 10:36:45 +0530

    Updated scripts

--- dev/DownloadCompatiblePlugins.py
+++ dev/DownloadCompatiblePlugins.py
@@ -6,14 +6,14 @@ Created on Mon Apr 21 15:07:54 2025
 """
 import pandas as pd
 import requests
-dataset = pd.read_excel("List_of_plugins_jenkins.xlsx", header=None)
+dataset = pd.read_excel("List_of_plugins.xlsx", header=None)
 
 destination = input("Where do you want the plugins to be downloaded? ")
 
 list_of_urls = dataset.iloc[:, :].values.tolist()
 
 for url_arr in list_of_urls:
-    url = url_arr[1]
+    url = url_arr[0]
     filename_arr = url.split("/")
     length = len(filename_arr)
     filename = filename_arr[length-1].replace(".hpi",".jpi")
--- dev/GetListOfPlugins.py
+++ dev/GetListOfPlugins.py
@@ -1,6 +1,6 @@
 import jenkins
 
-from xlwt import Workbook 
+import xlsxwriter
 
 class Plugins:
     def get_server_instance(_self):
@@ -13,22 +13,20 @@ class Plugins:
     def get_plugin_details(_self):
         try:
             server = _self.get_server_instance()
-            wb = Workbook() 
-          
-            #  add_sheet is used to create sheet. 
-            sheet = wb.add_sheet('Plugins') 
+
             destination = input("Path to save list of plugins:")
-            count = 0
+            workbook = xlsxwriter.Workbook(f"{destination}/List_of_plugins.xlsx")
+            worksheet = workbook.add_worksheet('Plugins')
+            row = 0
             for plugin in server.get_plugins().values():
                 download_url = f"https://updates.jenkins.io/download/plugins/{plugin['shortName']}/{plugin['version']}/{plugin['shortName']}.hpi"
-                sheet.write(count, 0, download_url)
-                count = count + 1
+                worksheet.write(row, 0, download_url)
+                row = row + 1
                 print(f"{download_url}")
-            
-            wb.save(f"{destination}/list_of_plugins.xls")
+            workbook.close()
         except Exception as e:
             print(f"An unexpected error occurred while fetching list of plugins from : {_self.jenkins_url} : {e}")
 
 plugins = Plugins()
 plugins.get_plugin_details()
-print("Successfuly created list_of_plugins.xls !")
+print("Successfuly created List_of_plugins.xlsx !")
